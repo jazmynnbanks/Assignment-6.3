0cb21fdfa50ad630f4142e262ea06f9db3e66520
jazmy <jazmy@JAZ-LAPTOP>
2025-05-15 16:52:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿LinkedList<string> myQueue=  new LinkedList<string>();
+
+myQueue.AddLast("Bryson");
+myQueue.AddLast("Brandin");
+myQueue.AddLast("James");
+
+Console.WriteLine("Current call queue: \n");
+
+foreach (var item in myQueue)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\nWorking through call list...\n");
+
+while (myQueue.Count > 0)
+{
+   string nextCaller= myQueue.First();
+    Console.Write($"Removing: {nextCaller}");
+    myQueue.RemoveFirst();
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nAll calls complete!");
+Console.ReadKey();
